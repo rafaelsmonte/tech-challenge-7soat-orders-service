455a5e76dc958cdf51c4738aec31a92dc8ca991d
vi049663 <vitor.silveira@intelbras.com.br>
2024-11-26 19:06:01 -0300

    fix: improve external APIs response handling

--- src/external/axios-client-http.external.ts
+++ src/external/axios-client-http.external.ts
@@ -14,15 +14,19 @@ export class AxiosClientHttp implements IClientHttp {
     });
   }
 
-  // TODO fix endpoints
-
   async createPayment(orderId: string, price: number): Promise<Payment> {
     const data = { orderId, price };
-    const response = await this.axiosClient.post('v1/payment', data, {
+    const response = await this.axiosClient.post('/payment', data, {
       baseURL: process.env.PAYMENTS_API_BASE_URL,
     });
 
-    const payment: Payment = response.data;
+    const payment = new Payment(
+      response.data.id,
+      response.data.orderId,
+      response.data.price,
+      response.data.pixQrCode,
+      response.data.pixQrCode64,
+    );
 
     return payment;
   }
@@ -31,11 +35,24 @@ export class AxiosClientHttp implements IClientHttp {
     productsWithQuantity: ProductWithQuantity[],
   ): Promise<Product[]> {
     const data = { productsWithQuantity };
-    const response = await this.axiosClient.post('v1/reserve', data, {
+    const response = await this.axiosClient.post('/stock/reserve', data, {
       baseURL: process.env.PRODUCTS_API_BASE_URL,
     });
 
-    const products: Product[] = response.data;
+    const products = response.data.map(
+      (product) =>
+        new Product(
+          product.id,
+          product.createdAt,
+          product.updatedAt,
+          product.name,
+          product.price,
+          product.description,
+          product.pictures,
+          product.categoryType,
+          product.quantity,
+        ),
+    );
 
     return products;
   }

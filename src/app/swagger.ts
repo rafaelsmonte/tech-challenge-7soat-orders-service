5d193d4eec74295f82b1af0d1cfa8186f2a7b84e
vi049663 <vitor.silveira@intelbras.com.br>
2024-12-01 20:37:14 -0300

    docs: add swagger json

--- src/app/swagger.ts
+++ src/app/swagger.ts
@@ -1,4 +1,4 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' });
 
 const port = 3000;
 const outputFile = './swagger-auto.json';
